dca6f108312f63849902b9f23a83b6706a5a776d
Guille Cánovas <755848@unizar.es>
2023-02-28 13:29:45 +0100

    quito los botones que sobraban

--- gym-tracker-app/screens/ExerciseScreen.jsx
+++ gym-tracker-app/screens/ExerciseScreen.jsx
@@ -27,16 +27,7 @@ const ExerciseScreen = () => {
 
         </ScrollView>
 
-        {/* Start Workout */}  
-        <View className="p-5 bg-white  space-y-4">
-          <TouchableOpacity 
-              onPress={() => navigation.navigate('Workout')}
-              className="rounded-full bg-[#001F2D] p-4 shadow-xl">
-              <Text className="text-center text-white text-lg font-bold">
-                  Start Workout
-              </Text>
-          </TouchableOpacity>
-        </View>
+      
 
         {/* Navbar */}
         <View>
--- gym-tracker-app/screens/ProfileScreen.jsx
+++ gym-tracker-app/screens/ProfileScreen.jsx
@@ -26,16 +26,7 @@ const ProfileScreen = () => {
 
       </ScrollView>
 
-      {/* Start Workout */}  
-      <View className="p-5 bg-white  space-y-4">
-        <TouchableOpacity 
-            onPress={() => navigation.navigate('Workout')}
-            className="rounded-full bg-[#001F2D] p-4 shadow-xl">
-            <Text className="text-center text-white text-lg font-bold">
-                Start Workout
-            </Text>
-        </TouchableOpacity>
-      </View>
+     
 
       {/* Navbar */}
       <View>
